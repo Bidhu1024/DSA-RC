36e6c882f438bedcd7fce1b1cbc27474635bc74e
Bidhu1024 <bidhu1024av@gmail.com>
2023-10-21 14:15:47 +0530

    selection sort

---
+++ Sorting/SelectionSort.js
@@ -0,0 +1,15 @@
+const selectionsort =(arr)=>{
+const n = arr.length;
+for (let i=0;i<n-1;i++){
+    let minIndex = i;
+    for(let j = i+1;j<n;j++){
+        if(arr[minIndex]>arr[j]){
+            minIndex = j;
+        }
+    }
+   if(minIndex !== i){
+    [arr[i],arr[minIndex]]=[arr[minIndex],arr[i]];
+   }
+}
+return arr
+}
--- callappbind/cab.js
+++ callappbind/cab.js
@@ -1,9 +1,26 @@
-let obj = {
-    name:"charlie"
+// let obj = {
+//     name:"charlie"
+// }
+
+// function say(){
+//     return "Hello " + this.name
+// }
+
+// console.log(say.call(obj))
+
+
+let age =20
+
+const person = {
+    name: 'Charlie',
+    age:"25",
+    getAge:function(){
+        return this.age
+    }
 }
 
-function say(){
-    return "Hello " + this.name
+var person2 = {
+    age:"24"
 }
 
-console.log(say.call(obj))
+console.log(person.getAge.call(person2));
