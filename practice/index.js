7cdea81a77ba2a3fcffcaf75bd29a251105797a0
Bidhu1024 <bidhu1024av@gmail.com>
2024-01-28 19:04:11 +0530

    Traverse

---
+++ practice/index.js
@@ -0,0 +1,12 @@
+
+
+
+var rotate = function(nums, k) {
+    if(nums.length>2) return
+
+    // for(let i = 0;i<nums.length;i++){
+
+    // }
+   let temp = nums.slice(k+1,nums.length-1);
+console.log(temp);
+};
---
+++ practice/opoa.js
@@ -0,0 +1,6 @@
+let arr = [1,2,3,4,5,6];
+
+let newel = 50;
+let pos = 3;
+arr[pos] = newel;
+console.log(arr)
