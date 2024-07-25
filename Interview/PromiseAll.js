bbb00fe9fa1377a6bda80f34698ee8dc01d15aa3
Bidhu1024 <bidhu1024av@gmail.com>
2024-07-25 09:21:30 +0530

    added polyfill

--- Interview/PromiseAll.js
+++ Interview/PromiseAll.js
@@ -5,6 +5,29 @@ let promise3 = new Promise((resolve,reject)=>{
 })
 
 
-Promise.all([promise1,promise2,promise3]).then((values)=>{
-    console.log(values);
-}).catch(err=>console.log("Err",err))
+// Promise.all([promise1,promise2,promise3]).then((values)=>{
+//     console.log(values);
+// }).catch(err=>console.log("Err",err))
+
+//polfill for promise.all
+
+let PromiseAll = (promises)=>{
+    let result = []
+    let count = 0;
+
+    return new Promise((resolve,reject)=>{
+        promises.forEach((promise,index)=>{
+            promise.then((value)=>{
+                result[index] = value;
+                count +=1;
+                if(count == promises.length){
+                    resolve(result);
+                }
+            }).catch(err=>reject(err))
+        })
+    })
+}
+
+PromiseAll([promise1,promise2,promise3]).then((data)=>{
+    console.log(data);
+}).catch(err=>console.log(err))
