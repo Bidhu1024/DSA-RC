16f6b2f0d8dae15b81ba33f21d40c2d08dd0248d
Bidhu1024 <bidhu1024av@gmail.com>
2024-07-26 10:28:27 +0530

    Promise.any()

---
+++ Interview/PromiseAny.js
@@ -0,0 +1,7 @@
+const Promise1 = Promise.reject("First Completed");
+const Promise2 = Promise.reject("Second Completed");
+const Promise3 = Promise.reject("Third Incomplete")
+
+Promise.any([Promise1,Promise2,Promise3]).then((data)=>{
+    console.log(data);
+}).catch(err=>console.log(err))
