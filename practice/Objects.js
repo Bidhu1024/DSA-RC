ff2654bc0f1d8290aaffb2b2cfac87e0b9e44740
Bidhu1024 <bidhu1024av@gmail.com>
2024-09-17 07:28:11 +0530

    Q2

--- practice/Objects.js
+++ practice/Objects.js
@@ -1,6 +1,33 @@
-const obj = {
-    a:"one",
-    b:"two",
-    a:"three"
+
+//Question 1
+
+// const obj = {
+//     a:"one",
+//     b:"two",
+//     a:"three"
+// }
+// console.log(obj)
+
+
+//Question 2
+
+//create a function that multiplies all numeric property values of nums by 2
+
+let nums = {
+    a:100,
+    b:200,
+    c:"Not a number"
 }
-console.log(obj)
+
+
+
+ let multiplyWithTwo = (obj)=>{
+    for(let key in obj){
+        if (typeof obj[key] === 'number'){
+
+            obj[key] *= 2;
+        }
+    }
+}
+multiplyWithTwo(nums)
+console.log(nums)
