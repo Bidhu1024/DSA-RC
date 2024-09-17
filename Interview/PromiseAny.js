8bcbd5d188f7402a7c96e1930a51efa2ba40c137
Bidhu1024 <bidhu1024av@gmail.com>
2024-09-17 07:19:49 +0530

    clg

--- Interview/PromiseAny.js
+++ Interview/PromiseAny.js
@@ -5,3 +5,5 @@ const Promise3 = Promise.reject("Third Incomplete")
 Promise.any([Promise1,Promise2,Promise3]).then((data)=>{
     console.log(data);
 }).catch(err=>console.log(err))
+
+// Polyfill for Promise.Any()
