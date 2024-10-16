80c2c48245d3c9b22af3ef5dd34725450a6ef3b9
Bidhu1024 <bidhu1024av@gmail.com>
2024-10-16 10:27:06 +0530

    Qustion #2 currying

--- practice/currying.js
+++ practice/currying.js
@@ -1,15 +1,45 @@
 
 // Question 1 Sum(1)(2),(3)
 
-function Sum(a){
+// function Sum(a){
+//     return function(b){
+//         return function(c){
+//             return a+b+c
+//         }
+//     }
+
+
+// }
+
+// let res = Sum(1)(2)(3)
+// console.log(res)
+
+
+// Question 2 
+
+// Evaluate Sum(2)(3) = > 5
+// Evaluate Multiply(2)(3) = > 6
+// Evaluate Divide(6)(3) = > 2
+// Evaluate Substract(3)(2) = > 1
+
+
+function Evaluate(operation){
+return function(a){
     return function(b){
-        return function(c){
-            return a+b+c
-        }
+        if(operation === 'add'){
+            return a+b;
+        }else if (operation === 'sub'){
+            return a-b;
+        } else if(operation ==="mul"){
+            return a*b;
+        }else if (operation === 'div'){
+            return a/b;
+        }else return 'invalid operation'
+
+
     }
-
-
+}
 }
 
-let res = Sum(1)(2)(3)
+let res = Evaluate('add')(5)(2)
 console.log(res)
