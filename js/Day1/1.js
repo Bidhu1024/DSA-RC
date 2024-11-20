35935d41d117cfd745f7f7e52f75a5f0ddf46715
Bidhu1024 <bidhu1024av@gmail.com>
2024-11-20 07:20:16 +0530

    type coercion

--- js/Day1/1.js
+++ js/Day1/1.js
@@ -2,3 +2,8 @@ console.log(typeof Object) //function
 console.log(typeof Array) //function
 console.log(typeof null) //Object
 console.log(typeof undefined) //undefined
+console.log(undefined + null) // implicit type coercion
+console.log(true - 1)
+console.log(true + null)
+console.log(false + null)
+
