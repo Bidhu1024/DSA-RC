ed83060033ab671c67b7ee6dffa977e69d8645eb
Bidhu1024 <bidhu1024av@gmail.com>
2024-11-20 09:20:01 +0530

    fix

--- js/Day1/1.js
+++ js/Day1/1.js
@@ -1,9 +1,17 @@
 console.log(typeof Object) //function
 console.log(typeof Array) //function
 console.log(typeof null) //Object
+console.log(typeof BigInt64Array) //Object
 console.log(typeof undefined) //undefined
 console.log(undefined + null) // implicit type coercion
 console.log(true - 1)
 console.log(true + null)
 console.log(false + null)
+
+console.log(typeof []) //object
+console.log(typeof {}) //object
+
+console.log({} =={})
+console.log([]+{})
+
 
