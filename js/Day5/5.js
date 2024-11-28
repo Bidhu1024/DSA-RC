98835ac7d140033eb860655fb22e3873bf5d6192
Bidhu1024 <bidhu1024av@gmail.com>
2024-11-28 07:47:26 +0530

    usefullness

--- js/Day5/5.js
+++ js/Day5/5.js
@@ -1,12 +1,32 @@
-function outer(){
+// function outer(){
+//     let count = 0;
+//     return function  inner(){
+//         count ++;
+//         return count;
+//     }
+// }
+
+// let closure = outer();
+
+// console.log(closure()) // 1
+// console.log(closure()) // 2
+
+// Q2: Why are closures useful?
+// A: Closures are essential for encapsulating data, creating private variables, memoization, and managing asynchronous behavior.
+
+function createCounter(){
     let count = 0;
-    return function  inner(){
-        count ++;
-        return count;
-    }
+    return {
+        increment:()=> ++count,
+        decrement: ()=>--count,
+        value: ()=>count
+    } 
 }
 
-let closure = outer();
-
-console.log(closure()) // 1
-console.log(closure()) // 2
+let counter = createCounter();
+console.log(counter.increment())
+console.log(counter.increment())
+console.log(counter.decrement())
+console.log(counter.increment())
+console.log(counter.decrement())
+console.log(counter.value())
