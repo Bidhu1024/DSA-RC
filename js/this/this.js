58d792a1d340f3cd1a77abfa5cf7029367403477
Bidhu1024 <bidhu1024av@gmail.com>
2024-12-04 09:47:48 +0530

    this inside object function

--- js/this/this.js
+++ js/this/this.js
@@ -1,2 +1,23 @@
 console.log(this) //{}-> node GEC
 console.log(this) //-> browser -> Window {window: Window, self: Window, document: document, name: '', location: Location, …}
+
+
+// check this inside the object
+
+let obj = {
+    name:"Gudu",
+    getName: function(){
+        console.log(this.name)
+    }
+}
+
+obj.getName()
+
+let obj1 = {
+    name:"Gudu",
+    getName:()=>{
+        console.log(this.name)
+    }
+}
+
+obj1.getName()
