20e08a64b6348ca2e205facd7e814b6588c0c127
Bidhu1024 <bidhu1024av@gmail.com>
2024-12-12 08:36:58 +0530

    Promise.race

--- js/AsyncJS/Promise.js
+++ js/AsyncJS/Promise.js
@@ -1,25 +1,40 @@
-const API = "https://api.github.com/users/Bidhu1024";
+// const API = "https://api.github.com/users/Bidhu1024";
 
 
 
-const response = fetch(API)
-console.log(response)
-response.then((data)=>{
-    console.log(data)
-}).catch(err=>console.log(err))
+// const response = fetch(API)
+// console.log(response)
+// response.then((data)=>{
+//     console.log(data)
+// }).catch(err=>console.log(err))
 
-const promise = new Promise((resolve,reject)=>{
+// const promise = new Promise((resolve,reject)=>{
+//   setTimeout(()=>{
+//     resolve("success")
+//   },5000)
+// })
+
+// promise.then(data=>console.log(data)).catch(err=>console.log(err)).finally(()=>console.log("promise is settled"))
+
+
+// const promise1 = Promise.resolve(10);
+// const promise2 = Promise.reject("Error!!!")
+
+// Promise.all([promise1,promise2])
+// .then(value=>console.log(value))
+// .catch(err=>console.log(err))
+
+//promise.race
+
+const promiseRace1 = new Promise((resolve,reject)=>{
   setTimeout(()=>{
-    resolve("success")
-  },5000)
+    resolve("First")
+  },1000)
+})
+const promiseRace2 = new Promise((resolve,reject)=>{
+  setTimeout(()=>{
+    resolve("Second")
+  },1100)
 })
 
-promise.then(data=>console.log(data)).catch(err=>console.log(err)).finally(()=>console.log("promise is settled"))
-
-
-const promise1 = Promise.resolve(10);
-const promise2 = Promise.reject("Error!!!")
-
-Promise.all([promise1,promise2])
-.then(value=>console.log(value))
-.catch(err=>console.log(err))
+Promise.race([promiseRace1,promiseRace2]).then(value=>console.log(value)).catch(err=>console.log(err))
